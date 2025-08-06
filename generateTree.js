375ff10f0c3fdff6091dba6f037fb18f31e2afaa
kh1012 <kh1012@midasit.com>
2025-08-06 13:37:02 +0900

    랜딩페이지 README.md 자동 생성 파이프라인 테스트 (md 출력 구조 변경)

--- generateTree.js
+++ generateTree.js
@@ -5,17 +5,19 @@ const owner = "kh1012"; // 리포지토리의 소유자
 const repo = "til"; // 리포지토리 이름
 const apiUrl = `https://api.github.com/repos/${owner}/${repo}/contents/`;
 
-async function generateTree(path = "") {
+async function generateTree(path = "", indent = "") {
   const res = await axios.get(apiUrl + path);
   const files = res.data;
   let tree = "";
 
   for (const file of files) {
     if (file.type === "dir") {
-      tree += `- [${file.name}](${file.html_url})\n`;
-      tree += await generateTree(file.path); // 재귀적으로 서브디렉토리 처리
+      // 디렉토리인 경우
+      tree += `${indent}[${file.name}](${file.html_url})\n`;
+      tree += await generateTree(file.path, indent + "  "); // 들여쓰기를 통해 서브디렉토리 생성
     } else {
-      tree += `- [${file.name}](${file.html_url})\n`;
+      // 파일인 경우
+      tree += `${indent}  ㄴ[${file.name}](${file.html_url})\n`; // 파일 앞에 'ㄴ'을 붙여 계층 표시
     }
   }
 
