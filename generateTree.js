3bf50dbf46ba7a51b5ff5286739d4a5101eee6db
kh1012 <kh1012@midasit.com>
2025-08-06 13:43:33 +0900

    랜딩페이지 README.md 자동 생성 파이프라인 테스트 (시크릿 추가)

--- generateTree.js
+++ generateTree.js
@@ -1,23 +1,28 @@
 const axios = require("axios");
 const fs = require("fs");
 
-const owner = "kh1012"; // 리포지토리의 소유자
-const repo = "til"; // 리포지토리 이름
+const owner = "kh1012";
+const repo = "til";
 const apiUrl = `https://api.github.com/repos/${owner}/${repo}/contents/`;
 
+// 인증을 위해 헤더에 Personal Access Token 추가
+const token = process.env.GH_PAT; // PAT을 환경 변수로 받음
+const headers = {
+  Authorization: `token ${token}`,
+  Accept: "application/vnd.github.v3+json",
+};
+
 async function generateTree(path = "", indent = "") {
-  const res = await axios.get(apiUrl + path);
+  const res = await axios.get(apiUrl + path, { headers });
   const files = res.data;
   let tree = "";
 
   for (const file of files) {
     if (file.type === "dir") {
-      // 디렉토리인 경우
       tree += `${indent}[${file.name}](${file.html_url})\n`;
-      tree += await generateTree(file.path, indent + "  "); // 들여쓰기를 통해 서브디렉토리 생성
+      tree += await generateTree(file.path, indent + "  "); // 서브 디렉토리 재귀 호출
     } else {
-      // 파일인 경우
-      tree += `${indent}  ㄴ[${file.name}](${file.html_url})\n`; // 파일 앞에 'ㄴ'을 붙여 계층 표시
+      tree += `${indent}  ㄴ[${file.name}](${file.html_url})\n`;
     }
   }
 
@@ -28,9 +33,9 @@ async function updateReadme() {
   const tree = await generateTree(); // 디렉토리 트리 생성
   const readmePath = "./README.md";
 
-  const readmeContent = `## Directory Tree\n\n${tree}`; // 디렉토리 트리를 markdown 형식으로
+  const readmeContent = `## Directory Tree\n\n${tree}`; // Markdown 형식
 
-  // README.md에 새로운 내용 작성
+  // README.md 파일 업데이트
   fs.writeFileSync(readmePath, readmeContent);
   console.log("README.md has been updated.");
 }
