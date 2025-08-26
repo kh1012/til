11b7b036812b7ec6a8515dea2cee9b4878348855
kh1012 <kh1012@midasit.com>
2025-08-26 19:55:07 +0900

    2장 완료, 기본 개발환경 세팅까지!

---
+++ 2025/00_js_ts_essential/codes/hello-world/index.js
@@ -0,0 +1,4 @@
+var colors = require("colors");
+
+console.log("Hello, World".rainbow);
+console.log("Hello, World".yellow);
---
+++ 2025/00_js_ts_essential/codes/simple/250826_Typescript.ts
@@ -0,0 +1,16 @@
+type Centimeter = number;
+type RainbowColor =
+  | "red"
+  | "orange"
+  | "yellow"
+  | "green"
+  | "blue"
+  | "indigo"
+  | "violet";
+
+let age = 10;
+let weight: number = 80;
+let height: Centimeter = 176;
+let color: RainbowColor = "orange";
+
+// 코드가 자신을 나타내는 표현력이 풍부해진다.
