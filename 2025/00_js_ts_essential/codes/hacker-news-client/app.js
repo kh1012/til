ee9b44641e4a5589083275489e309aa64781fe97
kh1012 <kh1012@midasit.com>
2025-08-28 13:33:42 +0900

    함수화, 라우터 구현

--- 2025/00_js_ts_essential/codes/hacker-news-client/app.js
+++ 2025/00_js_ts_essential/codes/hacker-news-client/app.js
@@ -11,14 +11,32 @@ function getData(url) {
   return JSON.parse(ajax.response);
 }
 
-const newsFeed = getData(NEWS_URL);
-const ul = document.createElement("ul");
+function newsFeed() {
+  const newsFeed = getData(NEWS_URL);
 
-window.addEventListener("hashchange", function () {
+  const newsList = [];
+
+  newsList.push("<ul>");
+
+  for (let i = 0; i < 10; i++) {
+    newsList.push(`
+    <li>
+      <a href="#${newsFeed[i].id}">
+        ${newsFeed[i].title} (${newsFeed[i].comments_count})
+      </a>
+    </li>
+  `);
+  }
+
+  newsList.push("</ul>");
+
+  container.innerHTML = newsList.join("");
+}
+
+function newsDetail() {
   const id = location.hash.substr(1);
-  const newsContent = getData(CONTENT_URL.replace("@id", id));
 
-  const title = this.document.createElement("h1");
+  const newsContent = getData(CONTENT_URL.replace("@id", id));
 
   container.innerHTML = `
     <h1>${newsContent.title}</h1>
@@ -27,22 +45,18 @@ window.addEventListener("hashchange", function () {
       <a href="#">목록으로</a>
     </div>
   `;
-});
-
-const newsList = [];
-
-newsList.push("<ul>");
-
-for (let i = 0; i < 10; i++) {
-  newsList.push(`
-    <li>
-      <a href="#${newsFeed[i].id}">
-        ${newsFeed[i].title} (${newsFeed[i].comments_count})
-      </a>
-    </li>
-  `);
 }
 
-newsList.push("</ul>");
+function router() {
+  const routePath = location.hash;
 
-container.innerHTML = newsList.join("");
+  if (routePath === "") {
+    newsFeed();
+  } else {
+    newsDetail();
+  }
+}
+
+window.addEventListener("hashchange", router);
+
+router();
