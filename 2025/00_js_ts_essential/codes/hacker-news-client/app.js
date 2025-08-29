e2e41fb073c4879ec838293c54a6b9fb468b4dfa
kh1012 <kh1012@midasit.com>
2025-08-29 13:29:42 +0900

    페이징 구현하기 (이전/다음 방어코드 적용)

--- 2025/00_js_ts_essential/codes/hacker-news-client/app.js
+++ 2025/00_js_ts_essential/codes/hacker-news-client/app.js
@@ -3,6 +3,9 @@ const ajax = new XMLHttpRequest();
 const content = document.createElement("div");
 const NEWS_URL = "https://api.hnpwa.com/v0/news/1.json";
 const CONTENT_URL = "https://api.hnpwa.com/v0/item/@id.json";
+const store = {
+  currentPage: 1,
+};
 
 function getData(url) {
   ajax.open("GET", url, false);
@@ -18,10 +21,10 @@ function newsFeed() {
 
   newsList.push("<ul>");
 
-  for (let i = 0; i < 10; i++) {
+  for (let i = (store.currentPage - 1) * 10; i < store.currentPage * 10; i++) {
     newsList.push(`
     <li>
-      <a href="#${newsFeed[i].id}">
+      <a href="#/show/${newsFeed[i].id}">
         ${newsFeed[i].title} (${newsFeed[i].comments_count})
       </a>
     </li>
@@ -29,12 +32,24 @@ function newsFeed() {
   }
 
   newsList.push("</ul>");
+  newsList.push(`
+    <div>
+      <a href="#/page/${
+        store.currentPage > 1 ? store.currentPage - 1 : 1
+      }">이전 페이지</a>
+      <a href="#/page/${
+        store.currentPage < Math.ceil(newsFeed.length / 10)
+          ? store.currentPage + 1
+          : Math.ceil(newsFeed.length / 10)
+      }">다음 페이지</a>
+    </div>
+  `);
 
   container.innerHTML = newsList.join("");
 }
 
 function newsDetail() {
-  const id = location.hash.substr(1);
+  const id = location.hash.substr(7);
 
   const newsContent = getData(CONTENT_URL.replace("@id", id));
 
@@ -42,7 +57,7 @@ function newsDetail() {
     <h1>${newsContent.title}</h1>
 
     <div>
-      <a href="#">목록으로</a>
+      <a href="#/page/${store.currentPage}">목록으로</a>
     </div>
   `;
 }
@@ -50,7 +65,12 @@ function newsDetail() {
 function router() {
   const routePath = location.hash;
 
+  console.log(routePath);
+
   if (routePath === "") {
+    newsFeed();
+  } else if (routePath.indexOf("#/page/") >= 0) {
+    store.currentPage = Number(routePath.substr(7));
     newsFeed();
   } else {
     newsDetail();
